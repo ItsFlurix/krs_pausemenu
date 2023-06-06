7d3f603e786fd3e32dd9ca61ebf54d7995f09dfe
ItsFlurix <61329362+ItsFlurix@users.noreply.github.com>
2023-06-06 19:24:48 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,8 +1,8 @@
 ESX = exports["es_extended"]:getSharedObject()
 
-print('^5[𝗞𝗥𝗦® │Development] Pausa Menu \n ^0')
+print('^5[𝗞𝗥𝗦® │Development] Menu de pausa \n ^0')
 
 RegisterNetEvent("krs_pausemenu:escidalgioco", function()
     local playerSource = source
-    DropPlayer(playerSource, "[𝗞𝗥𝗦® │Development] \nBuona giornata!")
+    DropPlayer(playerSource, "[nameserver] \n(Mensaje que quieras poner para cuando un jugador se desconecta)")
 end)
